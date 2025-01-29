894b22f4340c682821f7512e1e768366ccd3d147
maheshbea81 <maheshbea8.1@gmail.com>
2025-01-28 21:31:22 -0600

    Update sets.py

--- sets.py
+++ sets.py
@@ -3,5 +3,6 @@
 2. sets are created using set function or elements in {}
 3. set1.add(16) , will insert 16 to set1 , len(set1) will give length
 4. set1.clear() --> will delete all elements in set
-5. 
+5. set1.intersection(set2), set1.difference(set2), set1.union(set2) --> will give intersection, difference and union of two sets.
+6.
 '''
