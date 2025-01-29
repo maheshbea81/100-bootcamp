f22dd5731c2235d2d3914e119b98a663f009caca
maheshbea81 <maheshbea8.1@gmail.com>
2025-01-28 21:26:28 -0600

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,7 @@
+'''
+1. sets are unordered, mutable, unique lists, denoted with {}
+2. sets are created using set function or elements in {}
+3. set1.add(16) , will insert 16 to set1 , len(set1) will give length
+4. set1.clear() --> will delete all elements in set
+5. 
+'''
