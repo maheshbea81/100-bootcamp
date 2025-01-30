03f40ff7d907c43993f156e48fb0e88220b224a2
maheshbea81 <maheshbea8.1@gmail.com>
2025-01-29 21:51:57 -0600

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,14 @@
+'''
+1. tuples are immutable, i.e cannot add/remove any elements. 
+2. create tuple using tup=(), if there is only one element in tuple, you have to add , at the end of element, like tup=(9,) or tupl=('A',)
+3. tuples supports indexing 
+4. in comparision to frozen sets, frozensets are also immutable, but unique elements, whereas tuples doesn't have to be unique. 
+5. variable mapping and unmapping, tupl=('ABC''DEF','FGI'), we can assign elements to variables using single line like this (a,b,c)=tupl, this will assign a='ABC',b='DEF and c='FGI'.
+   this variable assignment works for lists,sets and strings ALSO. 
+6. (a,b,c)=(1,2,3) is easy way of defining multiple variables, this also works for sets, strings and lists. 
+7. a=(), b=[], if you want to know what builtin methods available for lists and tuples , use dir(a) or dir(b) will list ALL available methods. 
+8. tuples and frozensets usually have less methods. 
+9. len, min, max, tuple1+tuple2 and indexing works in tuples. 
+10. 
+
+'''
