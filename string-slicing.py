6684be3dca7c0be56735e0e23092425ba090b743
maheshbea81 <maheshbea8.1@gmail.com>
2025-02-05 22:09:03 -0600

    Update string-slicing.py

--- string-slicing.py
+++ string-slicing.py
@@ -5,6 +5,9 @@ name[START:STOP:STEP]   where STOP is not included in the output.
 if no ':' are specified , it is START position. name[0] --> will print m, name[-1]  will print y
 if 1 ':' and there are values before and after :, they are START and STOP , name[0:10]  --> mahesh red will be printed, remember STOP value is NOT included.
 if 2 ':' are specified, first value is START, second is STOP and third is STEP.
+Why Are Strings, Integers, and Floats Immutable?
+  When you assign a new value to a string, integer, or float, Python doesn't actually "modify" the original object.
+  Instead, it creates a new object in memory and updates the variable reference to point to this new object.           
 '''
 
 my_string = "Hello, World!"
