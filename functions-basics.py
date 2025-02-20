dadf2d74bcbcf5e0f50c95753fc631981d0a2fa6
vavillamahesh <65934071+vavillamahesh@users.noreply.github.com>
2025-02-19 22:01:45 -0600

    Create functions-basics.py

---
+++ functions-basics.py
@@ -0,0 +1,13 @@
+'''
+exceptions: 
+  ImportError
+  ModuleNotFoundError
+  ZeroDivisionError
+  NameError-->usually happens, when Variable is not defined, but trying to use 
+  TypeError --> when trying to perform operation on different unsupported data types
+  IndexError --> when trying to access out of bound index from either list, sets, tuples. 
+  functions are re-usable blocks of code.
+  docstring is descriptive comments right after function definition noted in "", if you type help(FUN_NAME) it will print comments in ""
+  params are the one's that are defined in function definition. args are the one's that are defined in function calling. 
+  
+'''
